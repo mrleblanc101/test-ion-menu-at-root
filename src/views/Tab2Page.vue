b67246a7d0bd8931c37b8a9d74c26d0fc38d8683
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2023-11-03 11:35:04 -0400

    Test case

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,23 +1,34 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Tab 2</ion-title>
+                <ion-buttons slot="end">
+                    <ion-menu-button></ion-menu-button>
+                </ion-buttons>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true" id="main-content">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Tab 2</ion-title>
+                </ion-toolbar>
+            </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
-    </ion-content>
-  </ion-page>
+            <ExploreContainer name="Tab 2 page" />
+        </ion-content>
+    </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonMenuButton,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,28 +1,57 @@
 <template>
-  <ion-page>
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
-        </ion-tab-button>
+    <ion-page>
+        <ion-tabs>
+            <ion-router-outlet></ion-router-outlet>
+            <ion-menu side="end" content-id="main-content">
+                <ion-header>
+                    <ion-toolbar>
+                        <ion-title>Menu</ion-title>
+                    </ion-toolbar>
+                </ion-header>
+                <ion-content class="ion-padding">
+                    <ion-list>
+                        <ion-item>
+                            <ion-label>Test</ion-label>
+                        </ion-item>
+                    </ion-list>
+                </ion-content>
+            </ion-menu>
+            <ion-tab-bar slot="bottom">
+                <ion-tab-button tab="tab1" href="/tabs/tab1">
+                    <ion-icon aria-hidden="true" :icon="triangle" />
+                    <ion-label>Tab 1</ion-label>
+                </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
+                <ion-tab-button tab="tab2" href="/tabs/tab2">
+                    <ion-icon aria-hidden="true" :icon="ellipse" />
+                    <ion-label>Tab 2</ion-label>
+                </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-  </ion-page>
+                <ion-tab-button tab="tab3" href="/tabs/tab3">
+                    <ion-icon aria-hidden="true" :icon="square" />
+                    <ion-label>Tab 3</ion-label>
+                </ion-tab-button>
+            </ion-tab-bar>
+        </ion-tabs>
+    </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+    IonMenu,
+    IonList,
+    IonItem,
+    IonTabBar,
+    IonTabButton,
+    IonTabs,
+    IonLabel,
+    IonIcon,
+    IonPage,
+    IonHeader,
+    IonContent,
+    IonToolbar,
+    IonTitle,
+    IonRouterOutlet,
+} from "@ionic/vue";
+import { ellipse, square, triangle } from "ionicons/icons";
 </script>
